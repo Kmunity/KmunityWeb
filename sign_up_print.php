577766e6f2b9affcd220ad7408d1d4fce8af9696
임병준 <imbyungjun@MacBook-Air.local>
2015-02-10 19:56:42 +0900

    aster

--- sign_up_print.php
+++ sign_up_print.php
@@ -1,11 +1,18 @@
+<html>
+<head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+</head>
+<body>
 <?
-	echo "아이디 : $id<br>";
-	echo "이름 : $name<br>";
-	echo "비밀번호 : $password<br>";
-	echo "비밀번호 확인 : $password_confirm<br>";
-	echo "성별 : $gender<br>";
-	echo "핸드폰 : $phone1 - $phone2 - $phone3<br>";
-	echo "자기소개 : $intro<br>";
-	echo "제목(hidden type에서 전달) : $title<br>";
+	$phone = $_POST['phone1']."-".$_POST['phone2']."-".$_POST['phone3'];
+	echo "아이디 : ".$_POST['id']."<br>";
+	echo "이름 : ".$_POST['name']."<br>";
+	echo "비밀번호 : ".$_POST['password']."<br>";
+	echo "비밀번호 확인 : ".$_POST['password_confirm']."<br>";
+	echo "성별 : ".$_POST['gender']."<br>";
+	echo "핸드폰 : $phone<br>";
+	echo "자기소개 : ".$_POST['intro']."<br>";
+	echo "제목(hidden type에서 전달) : ".$_POST['title']."<br>";
 ?>
+</body>
+</html>
