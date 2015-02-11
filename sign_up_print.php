0c345c4c5d4612006e8ac5a08f99940aa56b3e80
임병준 <imbyungjun@MacBook-Air.local>
2015-02-11 12:49:53 +0900

    fixed

--- sign_up_print.php
+++ sign_up_print.php
@@ -4,15 +4,14 @@
 </head>
 <body>
 <?
-	$phone = $_POST['phone1']."-".$_POST['phone2']."-".$_POST['phone3'];
-	echo "아이디 : ".$_POST['id']."<br>";
-	echo "이름 : ".$_POST['name']."<br>";
-	echo "비밀번호 : ".$_POST['password']."<br>";
-	echo "비밀번호 확인 : ".$_POST['password_confirm']."<br>";
-	echo "성별 : ".$_POST['gender']."<br>";
-	echo "핸드폰 : $phone<br>";
-	echo "자기소개 : ".$_POST['intro']."<br>";
-	echo "제목(hidden type에서 전달) : ".$_POST['title']."<br>";
+	echo "아이디 : $_POST[id]<br>";
+	echo "이름 : $_POST[name]<br>";
+	echo "비밀번호 : $_POST[password]<br>";
+	echo "비밀번호 확인 : $_POST[password_confirm]<br>";
+	echo "성별 : $_POST[gender]<br>";
+	echo "핸드폰 : $_POST[phone1] - $_POST[phone2] - $_POST[phone3]<br>";
+	echo "자기소개 : $_POST[intro]<br>";
+	echo "제목(hidden type에서 전달) : $_POST[title]<br>";
 ?>
 </body>
 </html>
